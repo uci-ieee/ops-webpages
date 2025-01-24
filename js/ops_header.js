f2001bd446054e13b6533aa3223b4f4f1a6b39c8
gavinn2 <gavinn2@uci.edu>
2025-01-24 15:11:39 -0800

    added header

--- js/ops_header.js
+++ js/ops_header.js
@@ -32,6 +32,7 @@ document.write(`
                             <li><a class="dropdown-item" href="./project_3.html">Project 3: RGB LED Wizard</a></li>
                             <li><a class="dropdown-item" href="./project_4.html">Project 4: Sundial</a></li>
                             <li><a class="dropdown-item" href="./project_5.html">Project 5: iPoduino v2.0</a></li>
+                            <li><a class="dropdown-item" href="./project_6.html">Project 6: Weather Station</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
