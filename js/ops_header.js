a5909eecbbe32df3d790f8a55f58088dad160ecd
Gavin Nguyen <gaivnn2@uci.edu>
2025-02-28 09:20:49 -0800

    ops 555 blinker pcb

--- js/ops_header.js
+++ js/ops_header.js
@@ -34,6 +34,7 @@ document.write(`
                             <li><a class="dropdown-item" href="./project_5.html">Project 5: iPoduino v2.0</a></li>
                             <li><a class="dropdown-item" href="./project_6.html">Project 6: Weather Station</a></li>
                             <li><a class="dropdown-item" href="./project_7.html">Project 7: Digital Stopwatch</a></li>
+                            <li><a class="dropdown-item" href="./project_8.html">Project 8: 555 Blinker PCB</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
