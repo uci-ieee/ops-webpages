93e8db995fe966d6c8af0b5d8161ed4aeadf87db
gavinn2 <gavinn2@uci.edu>
2025-04-04 14:14:50 -0700

    adding capstone to header

--- js/ops_header.js
+++ js/ops_header.js
@@ -35,6 +35,7 @@ document.write(`
                             <li><a class="dropdown-item" href="./project_6.html">Project 6: Weather Station</a></li>
                             <li><a class="dropdown-item" href="./project_7.html">Project 7: Digital Stopwatch</a></li>
                             <li><a class="dropdown-item" href="./project_8.html">Project 8: 555 Blinker PCB</a></li>
+                            <li><a class="dropdown-item" href="./project_capstone.html">Capstone Project</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
