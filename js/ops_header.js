0db38dc9352b5e572c82b68578739596fe572f00
Zicong Yu <kintamago>
2026-02-24 23:57:28 -0800

    added project 7 & 8 lecture videos and workshop slides

--- js/ops_header.js
+++ js/ops_header.js
@@ -34,9 +34,10 @@ document.write(`
                             <li><a class="dropdown-item" href="./project_5.html">Project 5: iPoduino v2.0</a></li>
                             <li><a class="dropdown-item" href="./project_6.html">Project 6: Weather Station</a></li>
                             <li><a class="dropdown-item" href="./project_7.html">Project 7: Digital Stopwatch</a></li>
-                            <li><a class="dropdown-item" href="./project_x.html">Project x: Etch a Sketch </a></li>
-                            <!-- 
+                            <li><a class="dropdown-item" href="./project_X.html">Project X: Etch a Sketch </a></li>
                             <li><a class="dropdown-item" href="./project_8.html">Project 8: 555 Blinker PCB</a></li>
+                            
+                            <!--
                             <li><a class="dropdown-item" href="./project_capstone.html">Capstone Project</a></li>
                             -->
                         </ul>
